18ef47d256eec81dc98367daefc938fad3a4755c
Aditi Mishra <asmishra@Aditis-MacBook-Air.local>
2017-12-04 15:39:17 -0600

    Updating

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,78 +1,43 @@
-// Run this function whenever user presses a key
-document.onkeyup = function(event) {
-// Determine which key was pressed
-var userGuess = event.key;
-}
-
-//while (guessesLeft > 0) {
-		//var i = 0;
-		//if userGuess===chosenArray[i] {
-			//guessesLeft--;
-
-		//}
-	//}
-//}
-
-//The Game Loop
-while (remainingLetters > 0) {
-	var guess = prompt("Guess a letter, or click cancel to stop playing");
-	
-
-//if (userGuess===chosenArray[i]) 
-
 var wordBank = ["galaxy", "alien", "pluto", "astronaut", "mercury", "star"];
 var chosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 var chosenArray = chosenWord.split("");
 
-//.split chosen word into an array, compare userGuess to array
-
-// Establish variables
-
-var wins = 0; 
-var winsPar = document.getElementById("wins");
-winsPar.innerHTML = "Wins: " + wins;
-
-// When player correctly guesses word, wins++
-
-
-// Show remaining guesses as numbers of letters in current word
-	var guessesLeft = chosenWord.length;
-
-	var remainingGuesses  = document.getElementById("remainingguesses");
-
+var guessesLeft = chosenWord.length;
+var remainingGuesses  = document.getElementById("remainingguesses");
 	remainingGuesses.innerHTML = "Number of Guesses Remaining: " + guessesLeft;
-	//var guessesPar = document.createElement("p");
-	//guessesPar.innerHTML = guessesLeft;
-	//remainingGuesses.appendChild(guessesPar);
 
 
-// Show appropriate number of dashes for current word
-	var currentWord = document.getElementById("currentword");
+var lettersGuessed = [];
+var letters = document.getElementById("lettersguessed");
+	letters.innerHTML = "Letters Already Guessed: " + lettersGuessed;
 
-	var answerArray = []
+
+
+var currentWord = document.getElementById("currentword");
+var answerArray = []
 
 	for (var i = 0; i < chosenWord.length; i++) {
 	answerArray[i] = "_";
 	}
 
 	currentWord.innerHTML = "Current Word: " + answerArray.join(" ");
-	//var wordPar = document.createElement("p");
-	//wordPar.innerHTML = answerArray;
-	//currentWord.appendChild(wordPar);
 
-	
-// Show letters that user has guessed
+var wins = 0; 
+var winsPar = document.getElementById("wins");
+winsPar.innerHTML = "Wins: " + wins;
 
-var lettersGuessed = [];
-var letters = document.getElementById("lettersguessed");
-letters.innerHTML = "Letters Already Guessed: " + lettersGuessed;
+
+//The Game Loop
+while (guessesLeft > 0) {
+}
+
 
 // Ask why this isn't working
-window.addEventListener('keypress', function (event) {
-    if (event.keyCode !== 13) {
-        lettersGuessed.push(event.key);
-    }
-}, false);
+//window.addEventListener('keypress', function (event) {
+    //if (event.keyCode !== 13) {
+        //lettersGuessed.push(event.key);
+    //}
+//}, false);
 
 
 
