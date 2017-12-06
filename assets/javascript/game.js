960736d0b69cd7d35af72f21fa68410067249983
Aditi Mishra <asmishra@Aditis-MacBook-Air.local>
2017-12-05 22:23:05 -0600

    updating js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,47 +3,49 @@ var chosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 var chosenArray = chosenWord.split("");
 
 var guessesLeft = chosenWord.length;
-var remainingGuesses  = document.getElementById("remainingguesses");
-	
-	function updateGuesses() {
-		remainingGuesses.innerHTML = "Number of Guesses Remaining: " + guessesLeft;
-	}
-
-
 var lettersGuessed = [];
-var letters = document.getElementById("lettersguessed");
-	
-	function lettersGuess () {
-		letters.innerHTML = "Letters Already Guessed: " + lettersGuessed;
-	}
-
-	document.onkeypress = function(e) {
-
-      var userGuess = e.key;
-  	  //lettersGuessed.push(userGuess);
-  	}
-
-  	function userGuess () {
-  		lettersGuessed.push(userGuess);
-  	}
-
-var currentWord = document.getElementById("currentword");
 var answerArray = []
-
-	for (var i = 0; i < chosenWord.length; i++) {
-	answerArray[i] = "_";
-	}
-
-	function currentWords () {
-		currentWord.innerHTML = "Current Word: " + answerArray.join(" ");
-	}
-
+var remainingGuesses  = document.getElementById("remainingguesses");
+var letters = document.getElementById("lettersguessed");
+var currentWord = document.getElementById("currentword");
 var wins = 0; 
 var winsPar = document.getElementById("wins");
 
-	function updateScore() {
-		winsPar.innerHTML = "Wins: " + wins;
-	}	
+// create dashes for the chosen word
+for (var i = 0; i < chosenWord.length; i++) {
+	answerArray[i] = "_";
+}
+
+// starts the game play
+// calls a bunch of functions
+startGame();
+
+function updateGuesses() {
+	remainingGuesses.innerHTML = "Number of Guesses Remaining: " + guessesLeft;
+}
+	
+function lettersGuess () {
+	letters.innerHTML = "Letters Already Guessed: " + lettersGuessed.join(" ");
+}
+
+document.onkeypress = function(e) {
+  	updateUserGuess(e.key);
+}
+
+function updateUserGuess (userGuess) {
+	// if statement to block multiple
+	// of the same letter
+	lettersGuessed.push(userGuess);
+	lettersGuess();
+}
+
+function currentWords () {
+	currentWord.innerHTML = "Current Word: " + answerArray.join(" ");
+}
+
+function updateScore() {
+	winsPar.innerHTML = "Wins: " + wins;
+}	
 
 // Create Game Loop Here
 
@@ -52,6 +54,7 @@ var winsPar = document.getElementById("wins");
 	// compare user guess to letters in array
 	// replace _ with letter if guessed correctly
 	// display updated array
+	// guessesLeft--
 	// wins++ when user guesses entire word
 	// show new word
 //}
@@ -67,11 +70,13 @@ var winsPar = document.getElementById("wins");
 //}, false);
 
 // Calling functions to start game
-updateGuesses();
-lettersGuess();
-currentWords();
-updateScore();
-userGuess();
+function startGame() {
+	updateGuesses();
+	lettersGuess();
+	currentWords();
+	updateScore();
+	updateUserGuess();
+}
 
 
 
